d14ed6acb5cfa0a078f0f22fc6f1da2a9081fe3e
Prince Vishwakarma <princevworld@gmail.com>
2021-09-13 23:56:00 +0530

    working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import Post from "./Components/Post";
+import Navbar from "./Components/Navbar";
+import Postview from "./Components/Postview";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 function App() {
+  const [posts, setPosts] = useState();
+  const data = async () => {
+    try {
+      const fetchdata = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const values = await fetchdata.json();
+      setPosts(values);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+  useEffect(() => {
+    data();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto px-4">
+      <BrowserRouter>
+        <Navbar />
+        <section className="text-gray-600 body-font">
+          <Switch>
+            <Route path="/" exact>
+              <Post post={posts} />
+            </Route>
+
+            <Route path="/:id" component={Postview} />
+          </Switch>
+        </section>
+      </BrowserRouter>
     </div>
   );
 }
