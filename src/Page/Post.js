ada3b50340a22f3be7c50a30d937c8d29a3a6f15
Prince Vishwakarma <princevworld@gmail.com>
2022-05-13 16:10:54 +0530

    remove truncate

--- src/Page/Post.js
+++ src/Page/Post.js
@@ -11,7 +11,7 @@ const Post = ({ post }) => {
             return (
               <div className="p-4 lg:w-1/3" key={item.id}>
                 <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-2 pb-2 rounded-lg overflow-hidden text-center relative">
-                  <h1 className="title-font sm:text-2xl text-xl text-left font-medium text-gray-900 mb-3 truncate">
+                  <h1 className="title-font sm:text-2xl text-xl text-left font-medium text-gray-900 mb-3">
                     {item.title}
                   </h1>
                   <img
